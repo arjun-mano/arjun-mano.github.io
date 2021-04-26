023e90a4d57a1744f9709b5aa6fd0e35bf553528
Arjun Mano <arjunmano10@gmail.com>
2021-04-26 20:57:52 +0530

    additional

---
+++ js/transition.js
@@ -0,0 +1,15 @@
+// import Highway from 'https://cdn.jsdelivr.net/npm/@dogstudio/highway@2.2.2/build/highway.min.js'
+
+// class Fade extends Highway.Transition{
+//     in(from, to,done){
+
+//         const tl = new TimelineLite()
+//         tl.fromTo(to, 0.5,{left:'-100%',top:'50%'},{left:'0%', onComplete:function(){
+//             done()
+//         }})
+//     }
+//     out({from, done}){
+//         done();
+//     }
+// }
+// export default Fade
