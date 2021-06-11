6f0600f3f1219c76999aaa188fced50ce2cc4619
Arjun Mano <arjunmano10@gmail.com>
2021-06-11 17:43:51 +0530

    skull

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-ul')[0]
+const navbarLinks = document.getElementsByClassName('navbar')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
